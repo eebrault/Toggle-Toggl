d6cd9ce186c134cc857249a5081f619e14c4595c
Thierry Rapillard <thierry.rapillard@alpsoft.ch>
2016-03-27 18:34:42 +0200

    Implement basic start action

--- src/IFTTT-Toggl/Controllers/TogglController.cs
+++ src/IFTTT-Toggl/Controllers/TogglController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using System;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
 using Microsoft.Extensions.Logging;
 using Toggl;
+using Toggl.Extensions;
+using Toggl.QueryObjects;
+using Toggl.Services;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,18 +18,37 @@ namespace IFTTT_Toggl.Controllers
 
 	    private ILogger<TogglController> Logger { get; set; }
 
-	    public TogglController(ILogger<TogglController> logger)
-	    {
-		    Logger = logger;
-		    TogglApiKey = Startup.Configuration["AppSettings:TogglApiKey"];
-	    }
+	    private TimeEntryService TimeEntryService { get; set; }
 
+	    public TogglController(ILogger<TogglController> logger)
+		{
+			Logger = logger;
+			TogglApiKey = Startup.Configuration["AppSettings:TogglApiKey"];
+			TimeEntryService = new TimeEntryService(TogglApiKey);
+
+			SetDefaultWorkspace();
+		}
+
+		private void SetDefaultWorkspace()
+		{
+			var workspaceService = new WorkspaceService(TogglApiKey);
+			var defaultWorkspace = workspaceService.List().FirstOrDefault();
+			if (defaultWorkspace?.Id != null) DefaultWorkspacedId = (int) defaultWorkspace.Id;
+		}
+
+		private int DefaultWorkspacedId { get; set; }
 
 	    // POST api/toggl/start
 		[HttpPost("start")]
         public void Start([FromBody]string value)
-        {
-
+		{
+			// Start a new time entry
+			TimeEntryService.Start(new TimeEntry()
+			{
+				Description = "IFTTT",
+				CreatedWith = "IFTTT Channel",
+				WorkspaceId = DefaultWorkspacedId
+			});
 		}
 
         [HttpPost]
